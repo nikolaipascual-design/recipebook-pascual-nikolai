4d72d7621313771f19e49482fec66daaea2aca58
nikolaipascual-design <nikolai.pascual@student.ateneo.edu>
2026-02-27 23:54:43 +0800

    Add files via upload

---
+++ recipebook/ledger/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete = models.CASCADE,
+        related_name = 'ingredients'
+    )
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete = models.CASCADE,
+        related_name = 'recipe'
+    )
